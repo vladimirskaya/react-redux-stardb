094c259ae12d68c15065cf0069b989b54a8056df
Nina Zurbulaeva <naina148@mail.ru>
2021-09-06 13:57:12 +0300

    feat: all code for class is ready

--- src/index.js
+++ src/index.js
@@ -12,12 +12,38 @@ class SwapiService {
   return await res.json();
   }
 
-  getAllPeople() {
-    return this.getResource(`/people/`);
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
   }
 
   getPerson(id) {
     return this.getResource(`/people/${id}`);
   }
 
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  async getAllStartships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+  
 }
+
+const swapi = new SwapiService();
+swapi.getAllPlanets().then((people) => {
+  people.forEach( p => {
+    console.log(p.name);
+  });
+})
