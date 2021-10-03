db5e0a9529844bdded29c10e4b0b7b35d020871f
Nina Zurbulaeva <naina148@mail.ru>
2021-10-03 09:26:50 +0300

    feat: refactoring

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -38,7 +38,8 @@ export default class PeoplePage extends Component {
         <div className="col-md-6">
           <ItemList
             onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}/>
+            getData={this.swapiService.getAllPeople}
+            renderItem={ (item) => item.name}/>
         </div>
         <div className="col-md-6">
           <PersonDetails personId={this.state.selectedPerson} />
