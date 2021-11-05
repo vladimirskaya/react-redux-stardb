dc8e454675d8a39ecdc4ceb5c6be7a03c57fea09
Nina Zurbulaeva <naina148@mail.ru>
2021-11-05 14:02:13 +0300

    feat: применение withRouter для отображения элемента из списка с -убираением самого списка -

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,6 +13,7 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 import './app.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { StarshipDetails } from '../sw-components';
 export default class App extends Component {
 
   state = {
@@ -47,7 +48,15 @@ export default class App extends Component {
                       exact />
                 <Route path="/people" component={PeoplePage} />
                 <Route path="/planets" component={PlanetsPage} />
-                <Route path="/starships" component={StarshipsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id" 
+                      render={ 
+                        ({ match }) => {  
+                          console.log(match);
+                          const { id } = match.params;
+                          return <StarshipDetails itemId={id}/> 
+                        }  
+                      } />
   
             </div>
           </Router>
--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import Row from '../row';
-import { StarshipList, StarshipDetails } from '../sw-components';
+import React from 'react';
+import { StarshipList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
 
-export default class StarshipsPage extends Component {
-    state = {
-        selectedItem: null
-    }
-    
-    onItemSelected = (selectedItem) => {
-        this.setState( {selectedItem} );
-    }
-    render() {
-        const { selectedItem } = this.state;
-        return (
-            <Row left={<StarshipList onItemSelected={this.onItemSelected}/>}
-                right={<StarshipDetails itemId={selectedItem} />}
-            />
-        );
-    };
+const StarshipsPage = ({history}) => {
+
+    return (
+        <StarshipList 
+            onItemSelected={(itemId) => {
+                const newPath = `/starships/${itemId}`;
+                history.push(newPath)
+
+
+            }}/>
+    );
 }
+export default withRouter(StarshipsPage);
